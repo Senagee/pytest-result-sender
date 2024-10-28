ea2a734576a2972e983dc6362a21f364266f27d1
Ken <2628255714@qq.com>
2024-10-28 16:28:44 +0800

    完成第一步，可以获取必要数据发送到企微

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,6 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pytest
+
+data = {}
 
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_collection_finish(session: pytest.Session):
+    print(session.items)
+
+
+@pytest.hookimpl(hookwrapper=True)
 def pytest_configure():
     # 配置文件加载完毕之后执行此方法、测试用例执行之前执行此方法
-    print(f"{datetime.now()} 现在开始执行测试用例")
+    data["start_time"] = datetime.now()
+    print(f"{datetime.now()} 开始执行测试用例")
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_unconfigure():
+    # 测试用例执行完之后执行此方法
+    data["end_time"] = datetime.now()
+    print(f"{datetime.now()} 测试结束")
+    data["duration"] = data["end_time"] - data["start_time"]
+    print(data["duration"])
+
+    assert timedelta(seconds=3) > data["duration"] >= timedelta(seconds=2.5)
